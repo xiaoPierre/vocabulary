eb0760910606897e5ddf0c412f5ee73d9d310aa5
ZHANG Erkang <kangzipo@outlook.com>
2016-05-04 16:00:45 +0200

    initial project version

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+
+
+# index view function suppressed for brevity
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form)
