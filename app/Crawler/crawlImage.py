94fa0f7f69bc15e53535c61f794ca8a6c5eb6a65
ZHANG Erkang <kangzipo@outlook.com>
2016-05-26 08:55:05 +0200

    crawlImage Added

---
+++ app/Crawler/crawlImage.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+def crawlImage(word):
+    urlBase = 'http://lemoteur.orange.fr/?module=lemoteur&bhv=images&kw='
+    url = urlBase + word
+    page = requests.get(url)
+    pageHTML = page.content.decode(page.encoding)
+    soup = BeautifulSoup(pageHTML, 'lxml')
+    image = soup.find('span', class_='imageElement')
+    if image:
+        adresseImage = image.find('img')['src']
+        return adresseImage
+    else:
+        return ''
+
+print(crawlImage('afadsfdsagdsa'))
